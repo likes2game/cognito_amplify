ed9379a14c65baa43ff3cda1b2c7782f36506b67
likes2game <likes2game@yahoo.com>
2024-03-07 00:47:46 -0600

    Image attempt 2

--- src/pages/Phoenix.js
+++ src/pages/Phoenix.js
@@ -8,9 +8,7 @@ const Phoenix = () => {
                 <div>
                     <h1>Successful Authentication, follow the link below to your app</h1>
                     <a href="https://main.d1vgowrgpfqwtj.amplifyapp.com/" target="_blank">Phoenix Pyre</a>
-                    <a href="https://main.d1vgowrgpfqwtj.amplifyapp.com/">
-                        <img src="%PUBLIC_URL%/Pyre-png.png" style="width:128px;height:128px;"></img>
-                    </a>
+                    <img src="%PUBLIC_URL%/Pyre-png.png" style="width:128px;height:128px;"></img>
                     <button onClick={signOut}>Sign Out</button>
                 </div>
             )}

eba6610893f0de8b2f2fe01cb5c414d3fd1d4dba
likes2game <likes2game@yahoo.com>
2024-03-07 09:16:13 -0600

    New redirect link

--- src/pages/Phoenix.js
+++ src/pages/Phoenix.js
@@ -7,7 +7,7 @@ const Phoenix = () => {
             {({ signOut }) => (
                 <div>
                     <h1>Successful Authentication, follow the link below to your app</h1>
-                    <a href="https://main.d1vgowrgpfqwtj.amplifyapp.com/" target="_blank">Phoenix Pyre</a>
+                    <a href="https://main.d1xrmgffcpyfxy.amplifyapp.com/" target="_blank">Phoenix Pyre</a>
                     <button onClick={signOut}>Sign Out</button>
                 </div>
             )}

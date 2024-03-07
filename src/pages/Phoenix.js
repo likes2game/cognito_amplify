c0fa0511a86320f5dd03bdf1c978e2bb29f36a58
likes2game <likes2game@yahoo.com>
2024-03-07 00:53:10 -0600

    Rolling Back to no image

--- src/pages/Phoenix.js
+++ src/pages/Phoenix.js
@@ -8,7 +8,6 @@ const Phoenix = () => {
                 <div>
                     <h1>Successful Authentication, follow the link below to your app</h1>
                     <a href="https://main.d1vgowrgpfqwtj.amplifyapp.com/" target="_blank">Phoenix Pyre</a>
-                    <img src="%PUBLIC_URL%/Pyre-png.png" style="width:128px;height:128px;"></img>
                     <button onClick={signOut}>Sign Out</button>
                 </div>
             )}

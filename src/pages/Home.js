10364298cec6820708b0b08ce2e79d9d22537efa
likes2game <likes2game@yahoo.com>
2024-03-07 12:59:01 -0600

    new picture

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
         <div>
-            <h1>Team Phoenix Home Page</h1>
+            <h1>Team Phoenix Home Page - Project 3</h1>
             <img src="https://cdn.wallpapersafari.com/95/6/npGvi6.jpg" alt="Team Phoenix Project 3" />
         </div>
     );

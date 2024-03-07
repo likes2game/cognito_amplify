3dfa4b069d8c127743765874286cf0198f0d7b44
likes2game <likes2game@yahoo.com>
2024-03-07 10:52:55 -0600

    Updated Home and About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,43 @@
 const About = () => {
-    return <h1>This is the About page</h1>;
+    return (
+        <div>
+            <h1>This is the Team Phoenix About page</h1>
+            <h2>Our Team Members:</h2>
+            <ul>
+                <li>
+                    <strong>Reynold Blue</strong> - Connect with Blue on{' '}
+                    <a href="https://www.linkedin.com/in/johndoe" target="_blank" rel="noopener noreferrer">
+                        LinkedIn
+                    </a>
+                </li>
+                <li>
+                    <strong>Jo-El Cruz</strong> - Find Jo-El at{' '}
+                    <a href="https://www.linkedin.com/in/janesmith" target="_blank" rel="noopener noreferrer">
+                        LinkedIn
+                    </a>
+                </li>
+                <li>
+                    <strong>Brent Likes</strong> - Visit Brent's LinkedIn profile{' '}
+                    <a href="https://www.linkedin.com/in/brent-likes" target="_blank" rel="noopener noreferrer">
+                        LinkedIn
+                    </a>
+                </li>
+                <li>
+                    <strong>Mikal Turgott</strong> - Connect with Mike on{' '}
+                    <a href="https://www.linkedin.com/in/mikal-turgott-8b946a29/" target="_blank" rel="noopener noreferrer">
+                        LinkedIn
+                    </a>
+                </li>
+            </ul>
+            <p>
+                For project management, check out our Jira page{' '}
+                <a href="https://skillstorm-phoenix.atlassian.net/jira/software/projects/P3/boards/1/timeline" target="_blank" rel="noopener noreferrer">
+                    Jira board
+                </a>
+                .
+            </p>
+        </div>
+    );
 };
 
 export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,10 @@
 const Home = () => {
-    return <h1>Home page</h1>;
+    return (
+        <div>
+            <h1>Home page</h1>
+            <img src="https://www.clipartmax.com/png/middle/446-4468176_phoenix-clipart-firebird-phoenix-bird-transparent-background.png" alt="Team Phoenix Project 3" />
+        </div>
+    );
 };
 
 export default Home;

8536bda174bed9ca4d9fcf2b9c239eaaf747beb8
likes2game <likes2game@yahoo.com>
2024-03-07 11:14:06 -0600

    Added a pic and LinkedIn and GitHub info to About

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,18 +2,18 @@ const About = () => {
     return (
         <div>
             <h1>This is the Team Phoenix About page</h1>
-            <img src="%PUBLIC_URL%/Pyre-png.png" alt="Team Phoenix" />
+            <img src="https://i.pinimg.com/1200x/b4/01/9a/b4019ae6b353620b0d15467dbac24018.jpg" alt="Team Phoenix" />
             <h2>Our Team Members:</h2>
             <ul>
                 <li>
                     <strong>Reynold Blue</strong> - Connect with Blue on{' '}
-                    <a href="https://www.linkedin.com/in/johndoe" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.linkedin.com/in/reynold-blue-13b640163/" target="_blank" rel="noopener noreferrer">
                         LinkedIn
                     </a>
                 </li>
                 <li>
                     <strong>Jo-El Cruz</strong> - Find Jo-El at{' '}
-                    <a href="https://www.linkedin.com/in/janesmith" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.linkedin.com/in/jo-el-cruz-gen2133/" target="_blank" rel="noopener noreferrer">
                         LinkedIn
                     </a>
                 </li>
@@ -21,6 +21,12 @@ const About = () => {
                     <strong>Brent Likes</strong> - Visit Brent's LinkedIn profile{' '}
                     <a href="https://www.linkedin.com/in/brent-likes" target="_blank" rel="noopener noreferrer">
                         LinkedIn
+                    </a>
+                </li>
+                <li>
+                    <strong></strong> - Check Out Brent's GitHub{' '}
+                    <a href="https://github.com/likes2game" target="_blank" rel="noopener noreferrer">
+                        GitHub
                     </a>
                 </li>
                 <li>
